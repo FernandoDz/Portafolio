b28608a6485ac519fcc00616e1b5aaf9f06fe001
Fernando Diaz <dfer4738@gmail.com>
2023-12-05 03:06:42 +0100

    changes in  layout

--- src/components/global/layout.vue
+++ src/components/global/layout.vue
@@ -54,16 +54,17 @@
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
                 <Router-link to="/"
-                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</Router-link>
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</Router-link>
                 <Router-link to="/about"
-                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</Router-link>
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</Router-link>
                 <Router-link to="/contact"
-                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</Router-link>
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact</Router-link>
                 <Router-link to="/projects"
-                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</Router-link>
+                  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Projects</Router-link>
               </div>
               <div class="py-6">
-                <a  href="https://drive.google.com/file/d/1BRMjYbKrnicoByqlc7TCDgrG3TRgk9sb/view?usp=sharing"  target="_blank"
+                <a href="https://drive.google.com/file/d/1BRMjYbKrnicoByqlc7TCDgrG3TRgk9sb/view?usp=sharing"
+                  target="_blank"
                   class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Download CV
                 </a>
