4f0be8d5ac11e89b57caa99f7b5fe36d9328d67a
Fernando Diaz <dfer4738@gmail.com>
2023-12-05 03:45:34 +0100

    footer fixed

--- src/components/global/layout.vue
+++ src/components/global/layout.vue
@@ -19,7 +19,8 @@ const toggleMenu = () => {
           </a>
         </div>
         <div class="flex lg:hidden">
-          <button @click="toggleMenu" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <button @click="toggleMenu" type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
             <span class="sr-only">Open main menu</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
               aria-hidden="true">
@@ -84,30 +85,29 @@ const toggleMenu = () => {
     <slot />
 
 
-    <footer class="mt-auto bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
 
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <Router-link  to="/" class="hover:underline me-4 md:me-6">Home</Router-link>
-            </li>
-            <li>
-              <Router-link to="/about" class="hover:underline me-4 md:me-6">About</Router-link>
-            </li>
-            <li>
-              <Router-link to="/projects" class="hover:underline me-4 md:me-6">Projects</Router-link>
-            </li>
-            <li>
-              <Router-link to="/contact" class="hover:underline">Contact</Router-link>
-            </li>
-          </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#"
-            class="hover:underline"></a>. All Rights Reserved.</span>
-      </div>
+
+    <footer
+      class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Router-link to="/"
+          class="hover:underline">Fernando Diaz™</Router-link>. All Rights Reserved.
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <Router-link to="/" class="hover:underline me-4 md:me-6">Home</Router-link>
+        </li>
+        <li>
+          <Router-link to="/about" class="hover:underline me-4 md:me-6">About</Router-link>
+        </li>
+        <li>
+          <Router-link to="/projects" class="hover:underline me-4 md:me-6">Projects</Router-link>
+        </li>
+        <li>
+          <Router-link to="/contact" class="hover:underline">Contact</Router-link>
+        </li>
+      </ul>
     </footer>
+
   </div>
 </template>
 
